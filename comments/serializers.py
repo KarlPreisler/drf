74120bc220422a653baea6fea5e5195ffdc3c5ec
KarlPreisler <Karlpreisler@gmail.com>
2023-03-08 12:42:38 +0000

    Added comment views and customized model by adding rating, configured urls

--- comments/serializers.py
+++ comments/serializers.py
@@ -20,7 +20,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content', 'rating'
+            'recipe', 'created_at', 'updated_at', 'content', 'rating'
         ]
 
 
@@ -29,4 +29,4 @@ class CommentDetailSerializer(CommentSerializer):
     Serializer for the Comment model used in Detail view
     Post is a read only field so that we dont have to set it on each update
     """
-    post = serializers.ReadOnlyField(source='post.id')
+    recipe = serializers.ReadOnlyField(source='recipe.id')

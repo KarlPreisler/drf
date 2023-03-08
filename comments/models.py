f4a77deb7cb0d39ea21febcf13e53da1f825240c
KarlPreisler <Karlpreisler@gmail.com>
2023-03-08 13:53:57 +0000

    add rating choices as tuple to limit rating to 0-5

--- comments/models.py
+++ comments/models.py
@@ -5,14 +5,22 @@ from recipes.models import Recipe
 
 class Comment(models.Model):
     """
-    Comment model, related to User and Post
+    Comment model, related to User and Recipe
     """
+    RATING_CHOICES = (
+        (0, '0'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    rating = models.IntegerField(default=5)
+    rating = models.IntegerField(choices=RATING_CHOICES, default=5)
 
     class Meta:
         ordering = ['-created_at']

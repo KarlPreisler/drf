37df4cb3b8993bef9a2beec1d02e191046b2a3cf
KarlPreisler <Karlpreisler@gmail.com>
2023-03-15 21:51:25 +0000

    remove second database function

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -155,10 +155,6 @@ DATABASES = {
     ))
 }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
